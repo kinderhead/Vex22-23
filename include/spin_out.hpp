2a12272ce40b7a650fc3f66cd3a85410355d2385
kinderhead <67707412+kinderhead@users.noreply.github.com>
2023-02-08 18:27:01 -0500

    Sad day for Joe

--- include/spin_out.hpp
+++ include/spin_out.hpp
@@ -17,14 +17,17 @@ using namespace std;
 class SpinOut : public VexMeet
 {
 public:
-    pros::Motor winch = pros::Motor(13);
-    pros::Motor intake = pros::Motor(15);
+    pros::Motor winch = pros::Motor(1);
+    pros::Motor intake = pros::Motor(5);
     ADIDigitalIn button = ADIDigitalIn('B');
     ADIDigitalOut launch = ADIDigitalOut('A');
     ADIDigitalOut stringShooter = ADIDigitalOut('C');
 
     int intakeSpeed = 0;
     bool pullingBack = false;
+
+    // Testing
+    bool winchSpin = false;
 
     SpinOut();
 
--- src/spin_out.cpp
+++ src/spin_out.cpp
@@ -12,31 +12,40 @@ void SpinOut::initialize()
     stringShooter.set_value(false);
     // Launch solenoid is reversed for some reason
     launch.set_value(true);
-    //shotPullBack(127);
+    shotPullBack(127);
 }
 
 void SpinOut::autonomous()
 {
+    robot->forward(3, 80, 2.2);
+    robot->turn(90, 50, 2.2);
+    robot->forward(21, 80, 2.2);
+    spinIntake(127);
+    robot->turn(90, 50, 2.2);
+    robot->forward(5, 50, 2.2);
+
+    pros::delay(400);
+    
+    spinIntake(0);
+    // robot->forward(5, 80, 2.2);
+    // spinIntake(127);
+    // pros::delay(1000);
+    // spinIntake(0);
 }
 
 void SpinOut::opcontrolLoop()
 {   
     if (robot->masterController.rArrow) stringShooter.set_value(true);
 
-    if (robot->masterController.leftBumper1) {
-        if (intakeSpeed == -INTAKE_SPEED) {
-            intakeSpeed = 0;
-        } else {
-            intakeSpeed = -INTAKE_SPEED;
-        }
+    if (robot->masterController.a) {
+        pullingBack = false;
+        winchSpin = !winchSpin;
     }
 
-    if (robot->masterController.leftBumper2) {
-        if (intakeSpeed == INTAKE_SPEED) {
-            intakeSpeed = 0;
-        } else {
-            intakeSpeed = INTAKE_SPEED;
-        }
+    if (winchSpin) {
+        spinWinch(-127);
+    } else {
+        spinWinch(0);
     }
 
     if (pullingBack) {
@@ -45,8 +54,14 @@ void SpinOut::opcontrolLoop()
         } else {
             pullingBack = false;
         }
+    }
+    
+    if (robot->masterController.leftBumper1) {
+        spinIntake(-INTAKE_SPEED);
+    } else if (robot->masterController.leftBumper2) {
+        spinIntake(INTAKE_SPEED);
     } else {
-        spinIntake(intakeSpeed);
+        spinIntake(0);
     }
 
     if (robot->masterController.y) launchDisks();
@@ -79,7 +94,8 @@ void SpinOut::launchDisks()
     spinWinch(0);
 
     launch.set_value(false);
-    pros::delay(100);
+    pros::delay(750);
+
     launch.set_value(true);
 
     shotPullBack(127);
